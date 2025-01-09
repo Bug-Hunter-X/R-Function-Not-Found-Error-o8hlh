1dd65beaff29df1238a334c4afb8b4144077d95d
BugHunter-X <askzenai@gmail.com>
2025-01-09 08:10:04 +0100

    Add bugSolution.r file

---
+++ bugSolution.r
@@ -0,0 +1,10 @@
+```r
+# Define the function or load the necessary package
+my_nonexistent_function <- function(x) {
+  x^2
+}
+
+#Now the function call will work
+result <- my_nonexistent_function(10)
+print(result) # Output: 100
+```
